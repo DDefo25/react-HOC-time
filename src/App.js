90322cc6b520f684b28b0d9aed75935de8c053a9
Maksim Pumé <94569453+DDefo25@users.noreply.github.com>
2023-05-29 15:07:24 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,60 +1,98 @@
-import React, {useState} from 'react';
-import './App.css'
-import DateTimePretty from './components/DateTimePretty';
+import React, { useState } from 'react';
+import "./App.css"
+import wrapComponent from './component/wrapComponent';
 
-function DateTime(props) {
+function New(props) {
     return (
-        <p className="date">{props.date}</p>
+        <div className="wrap-item wrap-item-new">
+            <span className="label">New!</span>
+            {props.children}
+        </div>
     )
-}
+};
 
-const DateTimeView = DateTimePretty(
-  DateTime,
-  'date'
-)
+function Popular(props) {
+    return (
+        <div className="wrap-item wrap-item-popular">
+            <span className="label">Popular!</span>
+            {props.children}
+        </div>
+    )
+};
+
+function Article(props) {
+    return (
+        <div className="item item-article">
+            <h3><a href="#">{props.title}</a></h3>
+            <p className="views">Прочтений: {props.views}</p>
+        </div>
+    )
+};
 
 function Video(props) {
     return (
-        <div className="video">
+        <div className="item item-video">
             <iframe src={props.url} frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
-            <DateTimeView date={props.date} />
+            <p className="views">Просмотров: {props.views}</p>
         </div>
     )
-}
+};
 
-function VideoList(props) {
-    return props.list.map(item => <Video url={item.url} date={item.date} />);
-}
+const WrapVideo = wrapComponent(New, Popular)(Video)
+const WrapArticle = wrapComponent(New, Popular)(Article)
+
+
+function List(props) {
+    return props.list.map(item => {
+        switch (item.type) {
+            case 'video':
+                return (
+                    <WrapVideo {...item} />
+                );
+
+            case 'article':
+                return (
+                    <WrapArticle {...item} />
+                );
+        }
+    });
+};
 
 export default function App() {
     const [list, setList] = useState([
         {
+            type: 'video',
             url: 'https://www.youtube.com/embed/rN6nlNC9WQA?rel=0&amp;controls=0&amp;showinfo=0',
-            date: '2023-04-31 13:24:00'
+            views: 50
         },
         {
+            type: 'video',
             url: 'https://www.youtube.com/embed/dVkK36KOcqs?rel=0&amp;controls=0&amp;showinfo=0',
-            date: '2023-05-29 12:10:00'
+            views: 12
         },
         {
-            url: 'https://www.youtube.com/embed/xGRjCa49C6U?rel=0&amp;controls=0&amp;showinfo=0',
-            date: '2023-05-29 15:10:00'
+            type: 'article',
+            title: 'Невероятные события в неизвестном поселке...',
+            views: 175
         },
         {
-            url: 'https://www.youtube.com/embed/RK1K2bCg4J8?rel=0&amp;controls=0&amp;showinfo=0',
-            date: '2018-01-03 12:10:00'
+            type: 'article',
+            title: 'Секретные данные были раскрыты!',
+            views: 1532
         },
         {
+            type: 'video',
             url: 'https://www.youtube.com/embed/TKmGU77INaM?rel=0&amp;controls=0&amp;showinfo=0',
-            date: '2018-01-01 16:17:00'
+            views: 4253
         },
         {
-            url: 'https://www.youtube.com/embed/TxbE79-1OSI?rel=0&amp;controls=0&amp;showinfo=0',
-            date: '2017-12-02 05:24:00'
+            type: 'article',
+            title: 'Кот Бегемот обладает невероятной...',
+            views: 12,
         },
     ]);
 
     return (
-        <VideoList list={list} />
+        <List list={list} />
     );
 }
